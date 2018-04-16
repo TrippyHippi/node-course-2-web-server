4abd4f2ae68860e5d3018b8993d48f6723327f11
Cody Eddins <cpeddins94@hotmail.com>
2018-04-16 14:29:28 -0400

    created project page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project Page',
+    pageMessage: 'This is my project portfolio',
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
